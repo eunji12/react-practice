c29410240dd65d8436778c69841797e7cfe38e98
박은지 <eunji@bag-eunjiui-MacBookPro.local>
2019-10-22 01:26:25 +0900

    Props 2.0-2.2

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Movie App React 2019
 # react-practice
+
+## 10/20
+- install node.js, npx-g, CRA, yarn  
+- 오 재밌군
+
+## 10/21
+- 야근하고 너무 피곤해서 1시간 정도 자다가 일어나서 해보기~
+- 마크다운도 안쓴지 오래되서 기억이 잘 안난다 . 안쓰면 간단한거도 항상 기억 안난다 자주 써야지
+- Component - html 반환 함수
+- Jsx - javascript & html 혼용
+```
+const frs = [1,2,3];
+frs.map(curr => {
+	return curr;
+})
+===
+frs.map(function(cur){
+	return curr;
+})
+```
+
+**  jsx에서 return 같은 레벨의 태그만( h2, span> 만 추가했더니 에러 발생 => 시작과 끝 태그가 있는걸로 묶어줘야한다
+```
+<div>
+	<h2>
+	<span>
+</div>
+```
+- Props/Prop-types
+
+## 10/22 (예정)
+- State, class Component
+- 리액트 개발자 도구 다운받기 
+https://reactjs.org/blog/2015/09/02/new-react-developer-tools.html#installation
--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
+
+function Food(props){
+  return (
+    <div>
+      <h3>I love {props.name}~</h3>
+      <span>rating : {props.rating}</span>
+    </div>
+  );
+
+}
+const foodILike = [
+  {
+    id :1
+    , name:"Kimchi"
+    , rating:5
+  }
+  ,{
+    id:2
+    , name:"Samgyeop"
+    , rating : 1000
+  }
+]
 function App() {
   return (
-    <h2>Hello</h2>
+    <div>
+      <h2>hello~</h2>
+      {
+        foodILike.map(dish => 
+          (
+           <Food key={dish.id} name={dish.name} rating={dish.rating}/>
+           )
+          )
+
+         /*foodILike.map(dish=>{
+          return <Food key={dish.id} name = {dish.name} rating = {dish.rating}/>;
+        })*/
+      
+       } 
+    </div>
     );
 }
+
+
 export default App;
+
